a22547d85861628750855fcabe648740bc833a9f
PC WHITE WOLF <a21310433@ceti.mx>
2023-11-28 14:40:41 -0600

    Version 1.5

--- src/Controller/main.java
+++ src/Controller/main.java
@@ -7,7 +7,7 @@ import java.rmi.RemoteException;
 public class main {
     public static void main(String[] args) throws RemoteException {
         MiClaseRemota miClaseRemota = new MiClaseRemota();
-        miClaseRemota.up("192.168.100.211");
+        miClaseRemota.up("1234");
         MainJframe mainJframe = new MainJframe();
         MainViewController mainViewController = new MainViewController(mainJframe, miClaseRemota);
         mainJframe.setVisible(true);
